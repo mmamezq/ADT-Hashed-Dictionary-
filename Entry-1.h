902e67689856b986bdb32e67163557aa8856b120
Maria Monica Marlene Amezquita <35312212+mmamezq@users.noreply.github.com>
2019-06-11 12:55:14 -0700

    Update Entry-1.h

--- Entry-1.h
+++ Entry-1.h
@@ -1,10 +1,3 @@
-//  Created by Frank M. Carrano and Tim Henry.
-//  Copyright (c) 2013 __Pearson Education__. All rights reserved.
-
-/** A class of entry objects for an array-based implementation of the ADT dictionary.
-    Listing 18-2.
- @file Entry.h */
-
 #ifndef _ENTRY
 #define _ENTRY
 
